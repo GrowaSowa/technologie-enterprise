acd516e06aa0524b9ff4079f502f09cd019f4d73
GrowaSowa <growasowa@gmail.com>
2025-05-05 13:40:08 +0200

    finished lab9

--- Complaints/Client/src/main/java/lab/app/Main.java
+++ Complaints/Client/src/main/java/lab/app/Main.java
@@ -3,7 +3,13 @@ package lab.app;
 import jakarta.json.*;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lab.entities.Complaint;
+
+import java.io.StringReader;
+import java.util.Set;
 
 public class Main {
     private static String url = "http://localhost:8080/Server-1.0-SNAPSHOT/";
@@ -12,9 +18,11 @@ public class Main {
         Client client = ClientBuilder.newClient();
         //getComplaintStatus(client, 601L);
         //getAllComplaints(client);
-        //getComplaint(client, 601L);
+        printComplaint(getComplaint(client, 604L));
 
-        getOpenComplaints(client);
+        //closeComplaint(client, 604L);
+        //openComplaint(client, 604L);
+        //getOpenComplaints(client);
         client.close();
     }
 
@@ -23,23 +31,27 @@ public class Main {
                 client.target(url + "api/complaints/")
                         .request(MediaType.APPLICATION_JSON)
                         .get(String.class);
-        System.out.println(res);
+        JsonStructure j = Json.createReader(new StringReader(res)).read();
+        printJsonArray(j.asJsonArray());
     }
 
     private static void getOpenComplaints(Client client) {
         String res =
-                client.target(url + "api/complaints?status=open")
+                client.target(url + "api/complaints")
+                        .queryParam("status", "open")
                         .request(MediaType.APPLICATION_JSON)
                         .get(String.class);
-        System.out.println(res);
+        JsonStructure j = Json.createReader(new StringReader(res)).read();
+        printJsonArray(j.asJsonArray());
     }
 
-    private static void getComplaint(Client client, Long id) {
-        String res =
-                client.target(url + "api/complaints/" + id)
+    private static Complaint getComplaint(Client client, Long id) {
+        Complaint complaint =
+                client.target(url + "api/complaints/")
+                        .path("{id}").resolveTemplate("id", id)
                         .request(MediaType.APPLICATION_JSON)
-                        .get(String.class);
-        System.out.println(res);
+                        .get(Complaint.class);
+        return complaint;
     }
 
     private static void getComplaintStatus(Client client, Long id) {
@@ -47,6 +59,51 @@ public class Main {
                 client.target(url + "api/complaints/" + id + "/status")
                         .request(MediaType.TEXT_PLAIN)
                         .get(String.class);
-        System.out.println("Status: " + res);
+        System.out.println("status: " + res);
+    }
+
+    private static void closeComplaint(Client client, Long id) {
+        Complaint complaint = getComplaint(client, id);
+        complaint.setStatus("closed");
+
+        Response response = client.target(url + "api/complaints")
+                .path("{id}").resolveTemplate("id", id)
+                .request().put(Entity.entity(complaint, MediaType.APPLICATION_JSON));
+        System.out.println("request status: " + response.getStatus());
+    }
+
+    private static void openComplaint(Client client, Long id) {
+        Complaint complaint = getComplaint(client, id);
+        complaint.setStatus("open");
+
+        Response response = client.target(url + "api/complaints")
+                .path("{id}").resolveTemplate("id", id)
+                .request().put(Entity.entity(complaint, MediaType.APPLICATION_JSON));
+        System.out.println("request status: " + response.getStatus());
+    }
+
+    private static void printJsonArray(JsonArray arr) {
+        int i = 0;
+        for(JsonValue val : arr) {
+            System.out.println(i + ":");
+            printJsonObject(val.asJsonObject());
+            System.out.println();
+            i++;
+        }
+    }
+
+    private static void printJsonObject(JsonObject obj) {
+        Set<String> keys = obj.keySet();
+        for(String key : keys) {
+            System.out.println(key + ": " + obj.get(key));
+        }
+    }
+
+    private static void printComplaint(Complaint complaint) {
+        System.out.println("id: " + complaint.getId());
+        System.out.println("date: " + complaint.getComplaintDate());
+        System.out.println("text: " + complaint.getComplaintText());
+        System.out.println("author: " + complaint.getAuthor());
+        System.out.println("status: " + complaint.getStatus());
     }
 }
